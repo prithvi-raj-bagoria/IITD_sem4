4867c74bd0930e55e2fb9cb0e5d4451298c1c414
Prithvi Raj <prithvirajbagoria.in@gmail.com>
2025-04-29 12:58:46 +0530

    Update a4.ml

--- COL226/Assg/a4/tests_4.ml
+++ COL226/Assg/a4/tests_4.ml
@@ -15,24 +15,27 @@ let test_case name f =
 open Krivine  (* Assuming your krivine.ml exposes Krivine module *)
 
 let test_krivine_var_lookup () =
-  let env = [Entry ("x", Clos (V "x", []))] in
-  let cl = Clos (V "x", env) in
-  let result = krivine_exec cl [] in
-  assert (unload result = V "x")
+  let env = [ ("x", Closure (V "x", []))] in
+  let cl = Closure (V "x", env) in
+  let final_state = krivine (cl, []) in (* Call krivine with tuple *)
+  let final_closure, _ = final_state in (* Extract final closure *)
+  assert (unload final_closure = V "x")
 
 let test_krivine_identity_function () =
   let id = Lam ("x", V "x") in
-  let env = [Entry ("y", Clos (V "y", []))] in
-  let cl = Clos (App (id, V "y"), env) in
-  let result = krivine_exec cl [] in
-  assert (unload result = V "y")
+  let env = [ ("y", Closure (V "y", []))] in
+  let cl = Closure (App (id, V "y"), env) in
+  let final_state = krivine (cl, []) in (* Call krivine with tuple *)
+  let final_closure, _ = final_state in (* Extract final closure *)
+  assert (unload final_closure = V "y")
 
 let test_krivine_nested_lambda () =
   let term = App (Lam ("x", Lam ("y", V "x")), V "z") in
-  let env = [Entry ("z", Clos (V "z", []))] in
-  let cl = Clos (term, env) in
-  let result = krivine_exec cl [] in
-  match unload result with
+  let env = [ ("z", Closure (V "z", []))] in
+  let cl = Closure (term, env) in
+  let final_state = krivine (cl, []) in (* Call krivine with tuple *)
+  let final_closure, _ = final_state in (* Extract final closure *)
+  match unload final_closure with
   | Lam ("y", V "z") -> ()
   | _ -> assert false
 
@@ -41,18 +44,20 @@ let test_krivine_application_of_functions () =
   let arg = Lam ("y", V "y") in
   let env = [] in
   let term = App (f, arg) in
-  let cl = Clos (term, env) in
-  let result = krivine_exec cl [] in
-  match unload result with
+  let cl = Closure (term, env) in
+  let final_state = krivine (cl, []) in (* Call krivine with tuple *)
+  let final_closure, _ = final_state in (* Extract final closure *)
+  match unload final_closure with
   | Lam ("y", V "y") -> ()
   | _ -> assert false
 
 let test_krivine_free_variable () =
   let term = Lam ("x", V "y") in
-  let env = [Entry ("z", Clos (V "z", []))] in
-  let cl = Clos (App (term, V "z"), env) in
-  let result = krivine_exec cl [] in
-  match unload result with
+  let env = [ ("z", Closure (V "z", []))] in
+  let cl = Closure (App (term, V "z"), env) in
+  let final_state = krivine (cl, []) in (* Call krivine with tuple *)
+  let final_closure, _ = final_state in (* Extract final closure *)
+  match unload final_closure with
   | V "y" -> ()
   | _ -> assert false
 
@@ -61,56 +66,54 @@ let test_krivine_free_variable () =
 open Secd  (* Assuming your secd.ml exposes SECD module *)
 
 let test_secd_var_lookup () =
-  let env = [ENTRY ("x", VCLOS ("x", [], []))] in
+  let env = [ ("x", Clos ("x", [], [], V "x")) ] in
   let s, e, c, d = ([], env, [LOOKUP "x"], []) in
-  let result = secd_execute s e c d in
+  let result = secd_machine (s, e, c, d) in
   match result with
-  | VCLOS ("x", [], _)  -> ()  (* Checking for the closure *)
+  | Clos ("x", [], _, _) -> ()
   | _ -> assert false
 
 let test_secd_simple_function_application () =
-  let lam = MKCLOS ("x", [LOOKUP "x"; RET]) in
+  let lam = MkCLOS ("x", [LOOKUP "x"; RET], Lam ("x", V "x")) in
   let prog = [lam; lam; APP] in
-  let result = secd_execute [] [] prog [] in
+  let result = secd_machine ([], [], prog, []) in
   match result with
-  | VCLOS ("x", [LOOKUP "x"; RET], _) -> ()  (* Checking for the closure *)
+  | Clos ("x", [LOOKUP "x"; RET], _, _) -> ()
   | _ -> assert false
 
 let test_secd_nested_functions () =
-  let inner = MKCLOS ("y", [LOOKUP "y"; RET]) in
-  let outer = MKCLOS ("x", [inner; RET]) in
+  let inner = MkCLOS ("y", [LOOKUP "y"; RET], Lam ("y", V "y")) in
+  let outer = MkCLOS ("x", [inner; RET], Lam ("x", Lam ("y", V "y"))) in
   let prog = [outer; outer; APP] in
-  let result = secd_execute [] [] prog [] in
+  let result = secd_machine ([], [], prog, []) in
   match result with
-  | VCLOS ("y", [LOOKUP "y"; RET], _) -> ()  (* Checking for the closure *)
+  | Clos ("y", [LOOKUP "y"; RET], _, _) -> ()
   | _ -> assert false
 
 let test_secd_application_ret () =
-  let lam = MKCLOS ("x", [LOOKUP "x"; RET]) in
+  let lam = MkCLOS ("x", [LOOKUP "x"; RET], Lam ("x", V "x")) in
   let prog = [lam; lam; APP] in
-  let result = secd_execute [] [] prog [] in
+  let result = secd_machine ([], [], prog, []) in
   match result with
-  | VCLOS ("x", [LOOKUP "x"; RET], _) -> ()  (* Checking for the closure *)
+  | Clos ("x", [LOOKUP "x"; RET], _, _) -> ()
   | _ -> assert false
 
-  let test_secd_closure_environment () =
-    let lam = MKCLOS ("x", [LOOKUP "x"; RET]) in
-    let env = [ENTRY ("y", VCLOS ("y", [], []))] in
-    let s, e, c, d = ([], env, [lam], []) in
-    let result = secd_execute s e c d in
-    match result with
-    | VCLOS ("x", [LOOKUP "x"; RET], closure_env) ->
-        (* Check that closure_env contains an ENTRY for "y" *)
-        let rec contains_y = function
-          | ENTRY ("y", _) :: _ -> true
-          | _ :: rest -> contains_y rest
-          | [] -> false
-        in
-        assert (contains_y closure_env)  (* Check for closure environment *)
-    | _ -> assert false
-  
+let test_secd_closure_environment () =
+  let lam = MkCLOS ("x", [LOOKUP "x"; RET], Lam ("x", V "x")) in
+  let env = [ ("y", Clos ("y", [], [], V "y")) ] in
+  let s, e, c, d = ([], env, [lam], []) in
+  let result = secd_machine (s, e, c, d) in
+  match result with
+  | Clos ("x", [LOOKUP "x"; RET], closure_env, _) ->
+      let rec contains_y = function
+        | ("y", _) :: _ -> true
+        | _ :: rest    -> contains_y rest
+        | []           -> false
+      in
+      assert (contains_y closure_env)
+  | _ -> assert false
+
 (* ---------- RUN ALL TESTS ---------- *)
- 
 
 let () =
   (* Krivine Tests *)
@@ -125,4 +128,4 @@ let () =
   test_case "SECD Simple Function Application" test_secd_simple_function_application;
   test_case "SECD Nested Functions" test_secd_nested_functions;
   test_case "SECD Application RET" test_secd_application_ret;
-  test_case "SECD Closure Environment" test_secd_closure_environment 
+  test_case "SECD Closure Environment" test_secd_closure_environment
